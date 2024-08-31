07a9071d34ec12e60e4f3823004e1d2f49ced899
WEBSHIVOM <shivompandey636@gmail.com>
2024-08-31 22:55:42 +0530

    updates

---
+++ src/main/java/com/webshivom/NewsRoomGlobal/NewsRoomGlobalApplication.java
@@ -0,0 +1,13 @@
+package com.webshivom.NewsRoomGlobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsRoomGlobalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsRoomGlobalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshivom/NewsRoomGlobal/NewsRoomGlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshivom.NewsRoomGlobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRoomGlobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
